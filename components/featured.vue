d6f7ebcd8897822f965439a4385b26d712b26532
imran <alimrancv870@gmail.com>
2023-12-10 15:48:55 +0600

    update

--- components/featured.vue
+++ components/featured.vue
@@ -4,7 +4,7 @@
             <div class="w-full">
                 <SectionTitle title="Empowering Data through Decentralization" subtitle="With decentralization at its core Crust Network is a purpose built layer 1 blockchain providing composable cloud storage solutions for developers, builders and end-users of all ecosystems." />
             </div>
-            <div class="flex -mx-3 px-3flex-wrap">
+            <div class="flex -mx-3 px-3 flex-wrap">
                 <div class="w-6/12 px-3" v-for="(item) in features" :key="item.id">
                     <div class="flex items-center flex-wrap">
                         <div class="w-[180px] h-[196px] flex items-center justify-center" v-html="item.icon"></div>
