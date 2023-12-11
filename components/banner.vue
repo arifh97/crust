b76486ee1062182f3dad39e202f5be7e74e1375d
imran <alimrancv870@gmail.com>
2023-12-11 22:00:50 +0600

    update

--- components/banner.vue
+++ components/banner.vue
@@ -1,22 +1,22 @@
 <template>
-    <div class="pt-[235px] pb-[200px] bg-[url('/img/hero-banner.png')] bg-cover bg-left-bottom reactive">
+    <div class="pt-[180px] md:pt-[235px] pb-[80px] md:pb-[200px] bg-[url('/img/hero-banner.png')] bg-cover bg-left-bottom reactive">
         <img src="/img/hero-banner-after.png" alt="" class="w-full h-auto absolute top-0 left-0 z-0">
         <div class="container px-3">
             <div class="flex -mx-3 justify-center">
-                <div class="w-10/12 px-3 text-center pb-[140px]">
-                    <div class="mb-24 px-[2%]">
-                        <h1 class="font-normal text-white text-[64px] leading-[1.28]">Complete Your Web3 Journey <strong class="font-bold">with Decentralized Storage</strong></h1>
+                <div class="w-full sm:w-10/12 px-3 text-center pb-[140px]">
+                    <div class="mb-[30px] md:mb-24 px-[2%]">
+                        <h1 class="font-normal text-white text-[28px] md:text-[45px] lg:text-[60px] xl:text-[64px] leading-[1.28]">Complete Your Web3 Journey <strong class="font-bold">with Decentralized Storage</strong></h1>
                     </div>
-                    <div class="w-8/12 flex items-center gap-8 mx-auto justify-center">
+                    <div class="w-11/12 sm:w-10/12 md:w-8/12 flex items-center gap-8 mx-auto justify-center">
                         <Button class="w-full">Build on Crust</Button>
                         <Button class="w-full bg-[#131313] border border-solid border-white border-opacity-[.30] hover:border-primary-full">Crust Mainnet</Button>
                     </div>
                 </div>
             </div>
-            <div class="flex -mx-3 justify-center">
-                <div class="w-3/12 px-3 text-center" v-for="item in bannerInfo" :key="item.title">
-                    <h3 class="text-[38px] font-bold leading-tight mb-3 text-white">{{ item.title }}</h3>
-                    <p class="mb-0 text-primary-500 uppercase font-medium text-opacity-90">{{ item.des }}</p>
+            <div class="flex flex-wrap -mx-3 justify-center">
+                <div class="w-1/2 md:w-1/3 lg:w-1/4 px-3 mb-10 md:mb-7 text-center" v-for="item in bannerInfo" :key="item.title">
+                    <h3 class="text-[18px] md:text-[38px] font-bold leading-tight mb-3 text-white">{{ item.title }}</h3>
+                    <p class="mb-0 text-primary-500 uppercase font-medium text-opacity-90 text-[14px] md:text-[18px]">{{ item.des }}</p>
                 </div>
             </div>
         </div>
--- components/featured.vue
+++ components/featured.vue
@@ -5,12 +5,12 @@
                 <SectionTitle title="Empowering Data through Decentralization" subtitle="With decentralization at its core Crust Network is a purpose built layer 1 blockchain providing composable cloud storage solutions for developers, builders and end-users of all ecosystems." />
             </div>
             <div class="flex -mx-3 px-3 flex-wrap">
-                <div class="w-6/12 px-3" v-for="(item) in features" :key="item.id">
-                    <div class="flex items-center flex-wrap mb-[139px]">
-                        <div class="decentralization_icon_box relative z-[1] w-[180px] h-[196px] flex items-center justify-center rounded-[10px]" v-html="item.icon"></div>
+                <div class="w-full lg:w-6/12 px-3" v-for="(item) in features" :key="item.id">
+                    <div class="flex items-center flex-wrap mb-[37px] md:mb-[70px] lg:mb-[139px]">
+                        <div class="decentralization_icon_box relative z-[1] w-[127px] md:w-[180px] h-[165px] md:h-[196px] flex items-center justify-center rounded-[10px] p-[25px] " v-html="item.icon"></div>
                         <div class="w-[calc(100%-180px)] pl-6">
-                            <h4 class="mb-3 leading-9 text-white text-opacity-90">{{ item.title }}</h4>
-                            <p class="text-[#B7B7B7] max-w-[332px]">{{ item.des }}</p>
+                            <h4 class="mb-3 leading-9 text-white text-opacity-90 text-[20px] md:text-[28px]">{{ item.title }}</h4>
+                            <p class="text-[#B7B7B7] max-w-[332px] text-[14px] md:text-[18px]">{{ item.des }}</p>
                         </div>
                     </div>
                 </div>
